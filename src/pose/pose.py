ec3636fa99e3350a98e0a4cbb2bb830fee3284b4
Abraham Linsenmayer <abe.linsenmayer@gmail.com>
2026-03-21 16:06:05 -0500

    Normalized poses, refined graphing visualization

--- src/pose/pose.py
+++ src/pose/pose.py
@@ -15,20 +15,25 @@ class Pose:
         """
 
         # Plot pose nodes
+        absMinY = abs(min(-node.y for node in self.nodes.values()))
+
         xs = [node.x for node in self.nodes.values()]
-        ys = [-node.y + 1 for node in self.nodes.values()]
+        ys = [-node.y + absMinY for node in self.nodes.values()]
         zs = [node.z for node in self.nodes.values()]
 
-        # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        fig, ax = plt.subplots()
-        ax.scatter(xs, ys)
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        ax.scatter(xs, zs, ys)
 
         # ax.set(yticklabels=[], xticklabels=[])
+
         ax.set_xlabel('X')
-        ax.set_xlim(0, 1)
+        ax.set_xlim(-2.5, 2.5)
+
         ax.set_ylabel('Y')
-        ax.set_ylim(0, 1)
-        # ax.set_zlabel('Z')
+        ax.set_ylim(-2.5, 2.5)
+
+        ax.set_zlabel('Z')
+        ax.set_zlim(0, 5)
 
         # A list of connections (start_name, end_name) based on MediaPipe Pose Map
         POSE_CONNECTIONS = [
@@ -82,8 +87,7 @@ class Pose:
         for connection in POSE_CONNECTIONS:
             start = self.nodes[connection[0]]
             end = self.nodes[connection[1]]
-            # ax.plot([start.x, end.x], [start.z, end.z], [-start.y, -end.y], c='b')
-            ax.plot([start.x, end.x], [-start.y + 1, -end.y + 1], c='b')
+            ax.plot([start.x, end.x], [start.z, end.z], [-start.y + absMinY, -end.y + absMinY], c='b')
 
         plt.show()
 
@@ -114,8 +118,8 @@ class Pose:
         self.vectorRepresentation = []
         
         # Iterate through pose landmarks
-        if landmarker_result.pose_landmarks and len(landmarker_result.pose_landmarks) > 0:
-            landmarks = landmarker_result.pose_landmarks[0]
+        if landmarker_result.pose_world_landmarks and len(landmarker_result.pose_world_landmarks) > 0:
+            landmarks = landmarker_result.pose_world_landmarks[0]
             
             # Create PoseNode objects first
             for i, landmark in enumerate(landmarks):
@@ -127,6 +131,9 @@ class Pose:
                         name=landmark_names[i]
                     )
                     self.nodes[pose_node.name] = pose_node
+            
+            # Normalize pose so shoulder distance is 1
+            self._normalize_shoulder_distance()
             
             # Calculate reference points after all nodes are created
             right_hip = None
@@ -174,6 +181,37 @@ class Pose:
                     self.vectorRepresentation.append(distance)
         
         return self
+    
+    def _normalize_shoulder_distance(self):
+        """
+        Normalize all pose coordinates so that the distance between right shoulder 
+        and left shoulder is always 1. All other coordinates are scaled proportionally.
+        """
+        if "right shoulder" not in self.nodes or "left shoulder" not in self.nodes:
+            return  # Cannot normalize if shoulders are not available
+        
+        # Get shoulder coordinates
+        right_shoulder = self.nodes["right shoulder"]
+        left_shoulder = self.nodes["left shoulder"]
+        
+        # Calculate current shoulder distance
+        shoulder_distance = (
+            (right_shoulder.x - left_shoulder.x) ** 2 +
+            (right_shoulder.y - left_shoulder.y) ** 2 +
+            (right_shoulder.z - left_shoulder.z) ** 2
+        ) ** 0.5
+        
+        if shoulder_distance == 0:
+            return  # Cannot normalize if shoulders are at the same position
+        
+        # Calculate scaling factor to make shoulder distance = 1
+        scale_factor = 1.0 / shoulder_distance
+        
+        # Scale all coordinates around the origin (0, 0, 0)
+        for node in self.nodes.values():
+            node.x *= scale_factor
+            node.y *= scale_factor
+            node.z *= scale_factor
     
     def __repr__(self):
         return f"Pose(nodes={len(self.nodes)}, vector_size={len(self.vectorRepresentation)})"
